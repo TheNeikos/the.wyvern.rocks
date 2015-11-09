acdef09b299c852d7bb4501a7cc74d0c87f0d8b8
Marcel Müller <neikos@neikos.email>
2015-11-09 19:28:01 +0100

    feat(User): Add User Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109173847) do
+ActiveRecord::Schema.define(version: 20151109181959) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "is_banned"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+  end
 
   create_table "versions", force: :cascade do |t|
     t.string   "item_type",                     null: false
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+email "MyString"
+password ""
+is_banned false
+  end
+
+end
