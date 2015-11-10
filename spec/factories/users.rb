aa8f2ad67ef5d1816616aa6eaf8f915960616958
Marcel Müller <neikos@neikos.email>
2015-11-10 13:38:12 +0100

    test(User): Add correct factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-email "MyString"
-password ""
-is_banned false
+    name "Flutter Shy"
+    email "flutters@shy.eu"
+    password "I have a Bunny!"
+    password_confirmation "I have a Bunny!"
+    is_banned false
   end
-
 end
