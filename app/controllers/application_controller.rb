63867f536901309b3dca005a2524ac7b2d2a2fbf
Marcel Müller <neikos@neikos.email>
2015-11-18 18:22:39 +0100

    chore(Pundit): Add good Pundit defaults

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include UsersHelper
+
+  after_action :verify_authorized, :except => :index
+  after_action :verify_policy_scoped, :only => :index
+
+  protected
+
+  def check_logged_in
+    redirect_to session_path if current_user
+  end
+
+  def check_logged_out
+    redirect_to new_session_path unless current_user
+  end
 end
