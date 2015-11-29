ec2783c7d7054629afed6f67c1333050050c833a
Marcel Müller <neikos@neikos.email>
2015-11-29 13:44:56 +0100

    feat(Topic): Add Topics to categories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -7,5 +7,13 @@ FactoryGirl.define do
           category.roles << Role.find_or_create_by(name: "Restricted")
       end
     end
+
+    trait :with_topics do
+      after(:create) do |category|
+        5.times do
+          category.topics << create(:topic)
+        end
+      end
+    end
   end
 end
