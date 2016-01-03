4f4c6c9a9bfc55be514061a3dce0ff969ba245a5
Marcel Müller <neikos@neikos.email>
2016-01-03 01:03:13 +0100

    feat(Markdown): Add markdown to posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def markdown(text)
+    options = {no_intra_emphasis: true, tables: true,
+               fenced_code_blocks: true, disable_indented_code_block: true,
+               strikethrough: true, superscript: true, underline: true,
+               footnotes: true, hard_wrap: true, safe_links_only: true}
+    renderer = Redcarpet::Render::HTML.new(options)
+    Redcarpet::Markdown.new(renderer, options).render(text.clone).html_safe
+  end
 end
