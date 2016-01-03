676df08ac433c0147d80c6fd51a1ea0002e75119
Marcel Müller <neikos@neikos.email>
2016-01-03 14:00:03 +0100

    fix(Post): Fix parsing HTML

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,9 @@ module ApplicationHelper
     options = {no_intra_emphasis: true, tables: true,
                fenced_code_blocks: true, disable_indented_code_block: true,
                strikethrough: true, superscript: true, underline: true,
-               footnotes: true, hard_wrap: true, safe_links_only: true}
+               footnotes: true, hard_wrap: true, safe_links_only: true,
+               escape_html: true}
     renderer = Redcarpet::Render::HTML.new(options)
-    Redcarpet::Markdown.new(renderer, options).render(text.clone).html_safe
+    Redcarpet::Markdown.new(renderer, options).render(text.freeze).html_safe
   end
 end
